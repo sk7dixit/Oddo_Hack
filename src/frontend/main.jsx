e1a479f1a294b5b3fd34ca3da34618c335957462
sk7dixit <shashwatdixit33@gmail.com>
2026-05-10 11:25:09 +0530

    Fix: Added missing BrowserRouter to main.jsx to resolve routing context error

--- src/frontend/main.jsx
+++ src/frontend/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import "@fontsource/inter";
@@ -10,6 +11,8 @@ import "@fontsource/inter/500.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
