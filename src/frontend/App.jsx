064c35b5a135de050e0923ba3a8d18884ea4ed5e
sk7dixit <shashwatdixit33@gmail.com>
2026-05-10 11:23:15 +0530

    Fix: Added BrowserRouter to provide routing context and resolve useRoutes() error

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import ChecklistPage from "./pages/ChecklistPage";
 import NotesPage from "./pages/NotesPage";
@@ -8,15 +8,17 @@ import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<ChecklistPage />} />
-        <Route path="/checklist" element={<ChecklistPage />} />
-        <Route path="/notes" element={<NotesPage />} />
-      </Route>
-      <Route path="/public/:tripId" element={<PublicTripPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<ChecklistPage />} />
+          <Route path="/checklist" element={<ChecklistPage />} />
+          <Route path="/notes" element={<NotesPage />} />
+        </Route>
+        <Route path="/public/:tripId" element={<PublicTripPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
